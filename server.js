63c4d6cfc78ac884ffbfc9e017044c76befa77be
danimisme <msubhanr53@gmail.com>
2025-08-15 00:25:51 +0700

    Refactor server.js to improve route definition and formatting

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: "localhost",
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello, Hapi!';
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "Hello, Hapi!";
+      },
+    },
+    {
+      method: "GET",
+      path: "/about",
+      handler: (request, h) => {
+        return "About Page";
+      },
     }
-  })
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
-}
+};
 
 init();
