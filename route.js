9524eec6f4f1c7ba84f3acff6d29100fcc1af7aa
danimisme <msubhanr53@gmail.com>
2025-08-15 00:52:46 +0700

    Refactor route definitions for improved readability and consistency

--- route.js
+++ route.js
@@ -1,18 +1,47 @@
 const routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
     },
-    {
-        method: 'GET',
-        path: '/about',
-        handler: (request, h) => {
-            return 'About page';
-        },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan metode ini";
     },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan metode ini";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = 'stranger'} = request.params;
+      return `Hallo, ${name}`;
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  }
 ];
- 
+
 module.exports = routes;
