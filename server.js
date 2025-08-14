964d5077f9268d5891822a579b98cbdc44937ebe
danimisme <msubhanr53@gmail.com>
2025-08-15 00:24:37 +0700

    Implement basic Hapi server setup with a GET route for the homepage

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Halo kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello, Hapi!';
+    }
+  })
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
